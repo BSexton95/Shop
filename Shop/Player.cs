d9424eb7262061659275e506b6b76e660d72d752
Brittnie <brittniesexton95@gmail.com>
2021-09-20 16:53:21 -0500

    Set up all functions for player and shop class

---
+++ Shop/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    class Player
+    {
+        private int _gold;
+        private Item[] _inventory;
+
+        public int Gold
+        {
+            get { return _gold; }
+        }
+
+        public Item[] Inventory
+        {
+            get { return _inventory; }
+        }
+
+        public Player(int )
+        {
+
+        }
+
+        public void Buy(Item )
+        {
+
+        }
+
+        public string[] GetItemNames()
+        {
+
+        }
+
+        public void Save(StreamWriter writer)
+        {
+
+        }
+
+        public bool Load(StreamReader reader)
+        {
+
+        }
+    }
+}
---
+++ Shop/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    class Shop
+    {
+        private int _gold;
+        private Item[] _inventory;
+
+        public Item[] Inventory
+        {
+            get { return _inventory; }
+        }
+
+        public Shop(Item[] )
+        {
+
+        }
+
+        public bool Sell(Player , int )
+    }
+}
