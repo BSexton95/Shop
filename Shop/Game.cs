886834ce141bcd55214d76156319414fe13a9646
Brittnie <brittniesexton95@gmail.com>
2021-09-20 16:34:56 -0500

    Added Item struct and set up functions to be used in game

---
+++ Shop/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    public struct Item
+    {
+        public int Cost;
+        public string Name;
+    }
+    class Game
+    {
+
+        public void Run()
+        {
+            
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+
+        private void InitalizeItems()
+        {
+
+        }
+
+        private int GetInput(string description, params string[] options)
+        {
+            string input = "";
+            int inputReceived = -1;
+
+            return intputReceived;
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private bool Load()
+        {
+
+        }
+
+        private void DisplayCurrentScene()
+        {
+
+        }
+
+        private void DisplayOpeningMenu()
+        {
+
+        }
+
+        private string[] GetShopMenuOptions()
+        {
+
+        }
+
+        private void DisplayShopMenu()
+        {
+
+        }
+
+
+    }
+}
