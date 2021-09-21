4284d31d7ca7a7b3d2e9013ab30781f6f87ba551
Brittnie <brittniesexton95@gmail.com>
2021-09-21 13:23:35 -0500

    Finished get input function

--- Shop/Game.cs
+++ Shop/Game.cs
@@ -48,12 +48,60 @@ namespace Shop
 
         }
 
+        /// <summary>
+        /// Function receives an input from player, the input being the players decision in game
+        /// </summary>
+        /// <param name="description">The context for the input</param>
+        /// <param name="options">The diffirent options the player has to choose from</param>
+        /// <returns>The players choose (The input from player)</returns>
         private int GetInput(string description, params string[] options)
         {
             string input = "";
             int inputReceived = -1;
 
-            return intputReceived;
+            while(inputReceived == -1)
+            {
+                //Prints the constext to console for player to see
+                Console.WriteLine(description);
+
+                //Prints every option for player to choose from
+                for(int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + options[i]);
+                }
+                Console.Write("> ");
+
+                //Get input from player
+                input = Console.ReadLine();
+
+                //If player typed an int...
+                if(int.TryParse(input, out inputReceived))
+                {
+                    //...decrement the input and check if it's within the bounds of the array
+                    inputReceived--;
+                    if(inputReceived < 0 || inputReceived >= options.Length)
+                    {
+                        //Set input received ot be the default value
+                        inputReceived = -1;
+
+                        //Display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey(true);
+                    }
+                }
+                //If the player did't type an int
+                else
+                {
+                    //Set input received to be the default value
+                    inputReceived = -1;
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey(true);
+                }
+
+                Console.Clear();
+            }
+
+            return inputReceived;
         }
 
         private void Save()
